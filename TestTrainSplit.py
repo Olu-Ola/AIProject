d31875d9a7113664ae2eb0ea2aafc4f1ede9b39d
Marwa Madni marwa.madni.001@student.uni.lu <marwa.madni4@gmail.com>
2024-11-30 14:42:17 +0100

    updated testtrainsplit.py

--- TestTrainSplit.py
+++ TestTrainSplit.py
@@ -1,26 +1,14 @@
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
-datasetDir = "./Dataset/IMDB/"
-file_path = datasetDir + "imdb_labelled.txt"
-output_csv = datasetDir + "output_file.csv"
+
+file_path = input("Give the path to txt file: ")
 #######################################
 # Step 1: Read the .txt file
 df = pd.read_csv(file_path, sep=r"\s{3,}", engine="python", header=None, names=["Input", "Label"])
 
+output_csv = input("Save the .csv file with name: ")
 # Step 2: Save to a CSV file
 df.to_csv(output_csv, index=False)
 
 print(f"CSV file saved as {output_csv}")
-
-#######################################
-
-# Load the CSV
-df = pd.read_csv(output_csv)
-
-# Split into train (first 900 rows) and test (last 100 rows)
-train_df = df.iloc[:10]  # it should be 900
-#test_df = df.iloc[-10:]  # it should be 10
-test_df = df.iloc[:10]
-train_df, val_df = train_test_split(train_df, test_size=0.2, random_state=42)
